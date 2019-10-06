90f46da3c76a5af0d625f0ccb5c56438f3a89f7e
Dustin <dustin.anthony.coffman@gmail.com>
2019-10-06 13:53:16 -0500

    added game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,22 @@
-var cardOne = "king";
-var cardTwo = "king"
-var cardThree = "queen";
-var cardFour = "queen";
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[2];
 
-console.log("the user has flipped" + " " + cardOne + " " + "and" + " " + cardTwo);
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+
+console.log("User flipped " + cardsInPlay);
+
+
+if (cardsInPlay.length === 2) {
+	
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You've found a match!");
+	}
+	else {
+		alert("Sorry, try again.");
+	}
+	
+}
