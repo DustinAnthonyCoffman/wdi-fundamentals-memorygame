b685895f2e734a5872408768a7f6b853d272642b
Dustin <dustin.anthony.coffman@gmail.com>
2019-10-06 16:33:59 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,22 +1,32 @@
-var cards = ['queen', 'queen', 'king', 'king'];
-var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[2];
+	var cards = ['queen', 'queen', 'king', 'king'];
+	var cardsInPlay = [];
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
+	function checkForMatch() {
 
-
-console.log("User flipped " + cardsInPlay);
-
-
-if (cardsInPlay.length === 2) {
-	
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You've found a match!");
-	}
-	else {
-		alert("Sorry, try again.");
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			
+			alert("You've found a match!");
+		}
+		else {
+		
+			alert("Sorry, try again.");
+		}
 	}
 	
-}
+	function flipCard(cardId) {
+
+		cardsInPlay.push(cards[cardId]);
+
+		console.log("User flipped " + cards[cardId]);
+
+		if (cardsInPlay.length === 2) {
+	
+			checkForMatch();
+	
+			}
+
+		}
+
+flipCard(0);
+flipCard(2);
+
